6fd0c0bffd08c49e461c7322275162801205efcf
ARUNITHA <55617338+arunitha99@users.noreply.github.com>
2022-09-12 17:41:42 +0530

    arunitha - loginComponent

--- src/components/LoginComponent.vue
+++ src/components/LoginComponent.vue
@@ -1,11 +1,47 @@
 <template>
-    <div class="login-container">
-        <legend>Login</legend>
-        
-        <input type="text" placeholder="email" v-model="user.emailId">
-        <input type="password" placeholder="password" v-model="user.password">
-
-    </div>
+  <div class="login-container">
+    <template>
+      <v-form ref="form" v-model="valid" lazy-validation>
+        <div class="login-heading">
+          <h2 class="heading">Login</h2>
+        </div>
+        <div class="form-input">
+          <v-text-field
+            v-model="user.username"
+            counter
+            :rules="[rules.required, rules.min, rules.max, rules.name]"
+            label="Name"
+            required
+          ></v-text-field>
+        </div>
+        <div class="form-input">
+          <v-text-field
+            v-model="user.password"
+            :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+            :rules="[rules.required, rules.pass, rules.max]"
+            :type="show ? 'text' : 'password'"
+            name="input-15-1"
+            label="Password"
+            counter
+            @click:append="show = !show"
+          ></v-text-field>
+        </div>
+        <div class="form-input">
+          <div class="centre">
+            <v-btn
+              :disabled="!valid"
+              color="success"
+              class="mr-4" small
+              @click="validate"
+            >
+              Login
+            </v-btn>
+            <v-btn small color="error" class="mr-4"  @click="reset"> Reset </v-btn>
+          </div>
+        </div>
+      </v-form>
+    </template>
+  </div>
 </template>
 
 
@@ -13,5 +49,63 @@
 
 
 <style scoped>
-    
+.heading {
+  text-align: center;
+  width: 100%;
+}
+.login-container {
+  background-color: #ffffff;
+  padding: 1em 1.5em;
+  border-radius: 1em;
+  width: 40%;
+  height: 45vh;
+  font-size: small;
+}
+.centre {
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: center;
+  font-size: x-small;
+}
+.form-input {
+  margin-bottom: 30px;
+  margin-top: 30px;
+  font-size: small;
+
+}
+.login-heading {
+    display:flex;
+    flex-flow:row nowrap;
+}
+@media screen and (min-width: 450px) and (max-width: 1000px) {
+  .login-container {
+    width: 65%;
+    height: 45vh;
+  }
+  .mr-4 {
+    font-size: x-small;
+    height: 30px !important;
+    margin-bottom: 10px;
+  }
+  .form-input {
+    margin-bottom: 30px;
+    margin-top: 30px;
+  }
+}
+@media screen and (max-width: 450px) {
+  .login-container {
+    width: 90%;
+    height: 45vh;
+  }
+  .mr-4 {
+    font-size: x-small;
+    height: 30px !important;
+    margin-bottom: 10px;
+  }
+
+  .form-input {
+    margin-bottom: 30px;
+    margin-top: 10px;
+  }
+}
 </style>
--- src/components/scripts/LoginComponent.js
+++ src/components/scripts/LoginComponent.js
@@ -1,14 +1,43 @@
 export default {
     name: 'LoginComponent',
-    data(){
-        return{
+    data() {
+        return {
             user: {
-                emailId: '',
+                username: '',
                 password: ''
-            }
+            },
+            rules: {
+                required: v => !!v || 'Required',
+                min: v => (v && v.length >= 3) || 'Min 3 characters',
+                max: v => (v && v.length <= 15) || 'Max 15 characters',
+                pass: v => {
+                    const pattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})/;
+                    return (
+                        pattern.test(v) ||
+                        "Min 8 characters with at least one uppercase letter,a lowercase letter, a number and a special character"
+                    );
+                },
+                name: v => {
+                    const namepattern = /[^A-Za-z0-9]/;
+                    return (
+                        (!namepattern.test(v)) ||
+                        "No special characters"
+                    )
+                }
+            },
+            show: false,
+            valid: true,
         }
     },
-    methods:{
-        
+    methods: {
+        validate() {
+            console.log(this.user.username);
+            console.log(this.user.password);
+            this.$refs.form.validate()
+        },
+        reset() {
+            console.log(this.$refs.form);
+            this.$refs.form.reset();
+        },
     }
 }
