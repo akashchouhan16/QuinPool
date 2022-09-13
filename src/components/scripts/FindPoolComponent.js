770613cbd0a744b733479d18c170a566f727a2cd
akashchouhan16 <akash.c1500@gmail.com>
2022-09-13 10:39:20 +0530

    init env for Google Maps API

--- src/components/scripts/FindPoolComponent.js
+++ src/components/scripts/FindPoolComponent.js
@@ -1,8 +1,9 @@
 import preloaderMixin from "@/mixins/preloader.mixin";
 import preloaderComponent from '@/components/customComponent/preloaderComponent.vue'
 
+
 import Vue from "vue";
-// import 'https://maps.googleapis.com/maps/api/js?key=AIzaSyB41DRUbKWJHPxaFjMAwdrzWzbVKartNGg&callback=initMap&v=weekly'
+// import 'https://maps.googleapis.com/maps/api/js?key=API_KEY&callback=initMap&v=weekly'
 export default {
   name: "FindPoolComponent",
   data() {
