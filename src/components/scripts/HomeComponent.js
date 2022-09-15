d866dfd2bef8d637ba95ac32dd86eb02afb62da6
akashchouhan16 <akash.c1500@gmail.com>
2022-09-15 09:59:55 +0530

    minor fix to Home

--- src/components/scripts/HomeComponent.js
+++ src/components/scripts/HomeComponent.js
@@ -1,29 +1,34 @@
-import Vue from 'vue'
-import preloaderMixin from '@/mixins/preloader.mixin'
-import preloaderComponent from '@/components/customComponent/preloaderComponent.vue'
-export default{
-    name: 'HomeComponent',
-    data(){
-        return {
-            isLoading: true
-        }
+import Vue from "vue";
+import preloaderMixin from "@/mixins/preloader.mixin";
+import preloaderComponent from "@/components/customComponent/preloaderComponent.vue";
+export default {
+  name: "HomeComponent",
+  data() {
+    return {
+      isLoading: true,
+    };
+  },
+  components: {
+    preloaderComponent,
+  },
+  mixins: [preloaderMixin],
+  methods: {
+    completeSetUp() {
+      Vue.$toast.warning("Setup could not be complete");
     },
-    components:{
-        preloaderComponent
+    initiatePool() {
+      // const userId = localStorage.getItem('userId') === null? this.$store.getters.getUser.userId : localStorage.getItem('userId');
+      this.$router.push({
+        path: "/initiatepool",
+        query: { redirect: `/initiatepool` },
+      });
     },
-    mixins: [preloaderMixin],
-    methods:{
-        completeSetUp(){
-            Vue.$toast.warning('Setup could not be complete')
-        },
-        initiatePool(){
-            
-            // const userId = localStorage.getItem('userId') === null? this.$store.getters.getUser.userId : localStorage.getItem('userId');
-            this.$router.push({path: '/initiatepool', query: { redirect: `/initiatepool`}});
-        },
-        findPool(){
-            // const userId = localStorage.getItem('userId') === null? this.$store.getters.getUser.userId : localStorage.getItem('userId');
-            this.$router.push({path: '/findpool', query: { redirect: `/findpool`}});
-        }
-    }
-}
+    findPool() {
+      // const userId = localStorage.getItem('userId') === null? this.$store.getters.getUser.userId : localStorage.getItem('userId');
+      this.$router.push({
+        path: "/findpool",
+        query: { redirect: `/findpool` },
+      });
+    },
+  },
+};
